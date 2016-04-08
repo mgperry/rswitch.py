00b5744d7f3d0237e95969a4e58929f76cc5dedd
mgperry <mgperry32@gmail.com>
2016-04-08 14:12:19 +0100

    added RVersion class

--- rswitch.py
+++ rswitch.py
@@ -1,132 +1,121 @@
 #!/usr/local/bin/python3
 from plistlib import load, InvalidFileException
-from os.path import basename, realpath, join, isdir, islink
+from os.path import dirname, basename, realpath, join, isdir, islink, abspath
 from os import walk, symlink, chdir, unlink
-from sys import argv, stdin
+from sys import argv, stdin, exit
 from re import match
 
+# optionally take from environment
 version_dir = "/Library/Frameworks/R.framework/Versions/"
 usage = "\nUsage: rswitch [version]"
 
-if not isdir(version_dir):
-    print("\n%s is not a directory.\n")
-    exit(1)
-
-r_versions = [{"v": x} for x in next(walk(version_dir))[1] if x != "Current"]
-
-if len(r_versions) == 0:
-    print("\nNo R versions found in %s\n" % version_dir)
-    exit(0)
-
-if islink(join(version_dir, "Current")):
-    current = basename(realpath(join(version_dir, "Current")))
-else:
-    current = None
-    print("\nNB: No Current version in use.")
-
-for r in r_versions:
-    if r["v"] == current:
-        r["current"] = True
-    else:
-        r["current"] = False
-    try:
-        fn = open(join(version_dir, r["v"], "Resources", "Info.plist"), 'rb')
-        pl = load(fn)
-        r["long"] = pl["CFBundleVersion"]
-    except (FileNotFoundError, InvalidFileException):
-        r["long"] = None
-
-if len(argv) > 2:
-    print("\nERROR: More than 1 argument supplied.")
-    print(usage)
-    print("")
-    exit(1)
-
-def print_version(i, r):
-    cols = []
-    v = r["v"]
-    if r["current"]:
-        v = "=> " + v
-    v = v.rjust(8)
-    cols.append("    [" + str(i) + "]")
-    cols.append(v)
-    if r["long"] is not None:
-        cols.append("   " + r["long"])
-    else:
-        cols.append("   " + "[incomplete installation]")
-    print(" ".join(cols))
-
-def print_all(r_v):
-    for i, r in enumerate(r_versions):
-        print_version(i+1, r_versions[i])
-
-if len(argv) == 1:
-    print("\nAvalable versions:\n")
-    print_all(r_versions)
-    print(usage)
-    print("")
-    exit(0)
-
 is_int = lambda x: match(r"^\d+$", x) is not None
-is_short = lambda x: match(r"^\d\.\d$", x) is not None
-is_long = lambda x: match(r"^\d\.\d\.\d$", x) is not None
 
-if len(argv) == 2:
-    v = argv[1]
-    if is_short(v):
-        if v in [r["v"] for r in r_versions]:
-            path = v
-        elif int(v[0]) > 3:
-            print("\nThis is a python script not a time machine\n")
-            exit(42)
+class RVersion:
+    def __init__(self, folder):
+        self.version = basename(folder)
+        self.r_home = dirname(folder)
+        try:
+            fn = open(join(folder, "Resources", "Info.plist"), 'rb')
+            pl = load(fn)
+            self.long = pl["CFBundleVersion"]
+        except (FileNotFoundError, InvalidFileException):
+            self.long = None
+        current = basename(realpath(join(dirname(folder), "Current")))
+        self.in_use = (current == self.version)
+
+    def __str__(self):
+        v = self.version
+        if self.in_use:
+            v = "=> " + v
+        v = v.rjust(8)
+        l = "   " + (self.long or "[incomplete installation]")
+        return v + l
+
+    def switch(self):
+        if self.in_use:
+            print("\nVersion %s already in use." % self.version)
         else:
-            print("\nVersion %s is not installed. Available versions are:\n" % v)
-            print_all(r_versions)
-            print("")
-            exit(0)
-    elif is_long(v):
-        if v in [r["long"] for r in r_versions]:
-            path = v[0:3]
-        elif v[0:3] in [r["v"] for r in r_versions]:
-            available = [r["long"] for r in r_versions if r["v"] == v[0:3]]
+            current = join(self.r_home, "Current")
+            islink(current) and unlink(current)
+            symlink(self.version, current)
+            print("\nSwitched to version %s" % self.version)
+
+        if self.long is None:
+            print("\nWARNING: Current R version does not appear to be correctly installed")
+
+    def has_version(self, v):
+        if self.version == v:
+            return True
+        elif self.long ==v:
+            return True
+        elif v[0:3] == self.version:
             msg = "\nVersion %s is not installed, however R %s is still available as version %s."
-            print(msg % (v, v[0:3], available[0]))
-            print("Switch to version %s? [Y/n]" % available[0])
+            print(msg % (v, v[0:3], self.long))
+            print("\nSwitch to version %s? [Y/n]" % self.long)
             answer = stdin.read(1)
             if answer in ["Y", "y", "\n"]:
-                path = v[0:3]
-            else:
-                exit(0)
-        else:
-            print("\nVersion %s is not installed. Available versions are:" % v)
-            print_all(r_versions)
-            exit(0)
-    elif is_int(v):
-        if v in map(str, range(1, len(r_versions)+1)):
-            path = r_versions[int(v)-1]["v"]
-        else:
-            print("\nThere are not that many versions of R installed. Available versions are:\n")
-            print_all(r_versions)
-            exit(0)
-    elif v.lower() == "devel":
-        path = r_versions[-1]["v"]
-    else:
-        print("\nERROR: Argument not understood\n")
-        print(usage)
+                return True
+
+        return False
+
+
+def version_info(lst):
+    print("\nAvalable versions:\n")
+    for i, r in enumerate(lst):
+        print("[{0}]".format(i+1).rjust(8), end="")
+        print(r)
+
+def main(argv):
+    if not isdir(version_dir):
+        print("\n%s is not a directory.\n")
         exit(1)
 
-    if path == current:
-        print("\nVersion %s already in use." % path)
-    else:
-        chdir(version_dir)
-        if current is not None:
-            unlink("Current")
-        symlink(path, "Current")
-        print("\nSwitched to version %s" % path)
+    r_versions = [RVersion(join(version_dir, x)) for x in next(walk(version_dir))[1] if x != "Current"]
+    r_versions.sort(key=lambda x: x.version)
 
-    if [r["long"] for r in r_versions if r["v"] == path][0] is None:
-        print("\nWARNING: Current R version does not appear to be correctly installed")
+    if len(r_versions) == 0:
+        print("\nNo R versions found in %s\n" % version_dir)
+        exit(0)
+
+    if not any([x.in_use for x in r_versions]):
+        print("\nNB: No Current version in use.")
+
+    if len(argv) == 1:
+        version_info(r_versions)
+        print(usage)
+        print("")
+        exit(0)
+
+    if len(argv) > 2:
+        print("\nERROR: More than 1 argument supplied.")
+        print(usage)
+        print("")
+        exit(1)
+
+    v = argv[1]
+
+    if is_int(v):
+        if int(v) < 1:
+            print("\nERROR: Index must be a positive integer.\n")
+            exit(1)
+        try:
+            r = r_versions[int(v)-1]
+        except IndexError:
+            print("\nThere are not that many versions of R installed. Available versions are:\n")
+            version_info(r_versions)
+            exit(0)
+    else:
+        r = next(filter(lambda x: x.has_version(v), r_versions), None)
+
+    if r is not None:
+        r.switch()
+    else:
+        print("\nVersion %s is not installed. Available versions are:" % v)
+        version_info(r_versions)
 
     print("")
     exit(0)
+
+main(argv)
 
